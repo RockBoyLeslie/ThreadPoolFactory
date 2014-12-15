9379aad260491f97a3ea19a745a91a301f33f91a
RockBoyLeslie <leslie.li@live.com>
2014-12-15 17:36:53 +0800

    修改sonar defects

--- src/main/java/com/pingan/common/thread/pool/ThreadPoolContextConstants.java
+++ src/main/java/com/pingan/common/thread/pool/ThreadPoolContextConstants.java
@@ -1,6 +1,6 @@
 package com.pingan.common.thread.pool;
 
-public class ThreadPoolContextConstants {
+public final class ThreadPoolContextConstants {
 
     public static final String AMESB_FUND_SERVICE_POOL = "amesbFundService";
 
